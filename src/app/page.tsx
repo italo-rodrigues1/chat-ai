d7da2985aba4e992d1933bba48c371f113b2f96d
italorodrigues <italosport110@hotmail.com>
2025-03-06 23:10:23 -0300

    feat: add auth for google

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 "use client";
 
 import { GalleryVerticalEnd } from "lucide-react";
-
 import { LoginForm } from "@/components/login-form";
-import { useEffect } from "react";
 
 export default function LoginPage() {
   return (
--- src/components/nav-user/index.tsx
+++ src/components/nav-user/index.tsx
@@ -1,13 +1,6 @@
 "use client";
 
-import {
-  BadgeCheck,
-  Bell,
-  ChevronsUpDown,
-  CreditCard,
-  LogOut,
-  Sparkles,
-} from "lucide-react";
+import { BadgeCheck, Bell, ChevronsUpDown, LogOut } from "lucide-react";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
@@ -25,6 +18,7 @@ import {
   SidebarMenuItem,
   useSidebar,
 } from "@/components/ui/sidebar";
+// import { Logout } from "@/lib/actions/auth";
 
 export function NavUser({
   user,
